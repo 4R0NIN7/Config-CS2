fbac18189e63354c866a497096c6d4ca53d4b647
Piotr Szczepanek <m4scorcher@gmail.com>
2024-03-30 21:37:02 +0100

    Update 30.03.2024 Removed hJump bind Changed sensitivity to 1.1 Added script to copy to the location of the game

---
+++ copy_script.bat
@@ -0,0 +1,10 @@
+@echo off
+set source_folder=C:\Users\PC\Desktop\Config CS2
+set destination_folder=C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg
+
+echo Copying files from %source_folder% to %destination_folder%...
+
+rem Use 'xcopy' command to copy files and directories recursively
+xcopy "%source_folder%\*" "%destination_folder%\" /s /i
+
+echo Files copied successfully.
